f9b0a755d8047345e6683a8c9d26a93ca751e6c2
mayconbalves <mayconmustaine@hotmail.com>
2019-09-10 22:04:20 -0300

    test(index): create tests for component

---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+import sensitiveWords from '.'
+
+test('replace blacklisted words with asterisks', () => (
+	expect(sensitiveWords('your speak portuguese ?', ['portuguese']))
+	.toBe('your speak **** ?')
+))
+
+test('replace blacklisted words with asterisks', () => (
+	expect(sensitiveWords(
+	'your speak portuguese ? do you dont speak portuguese ?', ['portuguese']))
+	.toBe('your speak **** ? do you dont speak **** ?')
+))
