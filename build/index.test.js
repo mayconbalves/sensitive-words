5670f148a9ee91183de4ab0fa22d2a9c118c6d53
Romulo Pransteter <r.pransteter@gmail.com>
2019-10-25 11:29:12 -0300

    feat(*): Implement new param to hide sensitive word.

--- build/index.test.js
+++ build/index.test.js
@@ -7,9 +7,17 @@ var _2 = _interopRequireDefault(_);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 test('replace blacklisted words with asterisks', function () {
-	expect((0, _2.default)('your speak portuguese ?', ['portuguese'])).toBe('your speak **** ?');
+	return expect((0, _2.default)('your speak portuguese ?', ['portuguese'])).toBe('your speak **** ?');
 });
 
 test('replace blacklisted words with asterisks', function () {
 	return expect((0, _2.default)('your speak portuguese ? do you dont speak portuguese ?', ['portuguese'])).toBe('your speak **** ? do you dont speak **** ?');
+});
+
+test('replace blacklisted words with asterisks and another substitued string', function () {
+	return expect((0, _2.default)('your speak portuguese ?', ['portuguese'], '[hidden_text]')).toBe('your speak [hidden_text] ?');
+});
+
+test('replace blacklisted words with asterisks and another substitued string', function () {
+	return expect((0, _2.default)('your speak portuguese ? do you dont speak portuguese ?', ['portuguese'], '[hidden_text]')).toBe('your speak [hidden_text] ? do you dont speak [hidden_text] ?');
 });
--- src/index.test.js
+++ src/index.test.js
@@ -10,3 +10,14 @@ test('replace blacklisted words with asterisks', () => (
 	'your speak portuguese ? do you dont speak portuguese ?', ['portuguese']))
 	.toBe('your speak **** ? do you dont speak **** ?')
 ))
+
+test('replace blacklisted words with asterisks and another substitued string', () => (
+	expect(sensitiveWords('your speak portuguese ?', ['portuguese'], '[hidden_text]'))
+	.toBe('your speak [hidden_text] ?')
+))
+
+test('replace blacklisted words with asterisks and another substitued string', () => (
+	expect(sensitiveWords(
+	'your speak portuguese ? do you dont speak portuguese ?', ['portuguese'], '[hidden_text]'))
+	.toBe('your speak [hidden_text] ? do you dont speak [hidden_text] ?')
+))
